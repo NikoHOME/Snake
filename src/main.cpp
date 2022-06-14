a0790748804f886dc16083c8a416b14947a82d74
Nikos <wiktor.gawronski@gmail.com>
2022-06-14 13:27:26 +0200

    fix basic rendering

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ const char* fragmentShaderSource2 = "#version 330 core\n"
 //variables
 
 auto currentTime = checktime;
-int tickspersecond = 1000/5;
+int tickspersecond = 1000/5,boostLimit=1000/8;
 int dir=0;
 
     //Up 0
@@ -194,34 +194,39 @@ int main()
 
 
 	struct nodes{
+		GLfloat x1,y1,z1,
+				x2,y2,z2,
+				x3,y3,z3,
+				x4,y4,z4;
 
-		GLfloat x1,y1,z1,blk1,
-				x2,y2,z2,blk2,
-				x3,y3,z3,blk3,
-				x4,y4,z5,blk4;
+	};
+	struct indices{
+
+		unsigned int i1,i2,i3,i4,i5,i6;
 
 	};
 
 	nodes vertices[] = 
 	{
-		{-0.9f,	0.45f,	0.0f, 1.0f,
-		0.9f,	0.45f,	0.0f, 1.0f,
-		-0.9f,	-0.9f,	0.0f, 1.0f,
-		0.9f,	-0.9f,	0.0f, 1.0f}
+		{-0.9f,	0.45f,	0.0f,
+		0.9f,	0.45f,	0.0f,
+		-0.9f,	-0.9f,	0.0f,
+		0.9f,	-0.9f,	0.0f}
 	};
 
 	std::vector <nodes> snakeVertices;
+	std::vector <indices> snakeIndices;
 	std::vector <nodes> fruitVertices;
 	fruitVertices.push_back
 	({
-			-0.585f, -0.855f, 0.0f, 1.0f,
-			-0.54f, -0.855f, 0.0f, 1.0f,
-			-0.585f, -0.9f, 0.0f, 1.0f,
-			-0.54f, -0.9f, 0.0f, 1.0f
+			-0.585f, -0.855f, 0.0f,
+			-0.54f, -0.855f, 0.0f,
+			-0.585f, -0.9f, 0.0f,
+			-0.54f, -0.9f, 0.0f
 	});
 
 	//create vertex buffer and vertex array
-	GLuint VAO[3],VBO[3]; //0-background 1-snake 2-fruit
+	GLuint VAO[3],VBO[3],EBO; //0-background 1-snake 2-fruit
 	//generate VAO and VBO
 	glGenVertexArrays(1, &VAO[0]);
 	glGenBuffers(1,&VBO[0]);
@@ -231,7 +236,7 @@ int main()
 	//aquire the vertices data
 	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 	//specify the data format
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE,0, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,0, 0);
 	//enable the VAO index 0
 	glEnableVertexAttribArray(0);
 
@@ -242,28 +247,33 @@ int main()
 	glGenBuffers(1,&VBO[1]);
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER,VBO[1]);
-	glBufferData(GL_ARRAY_BUFFER,sizeof(snakeVertices),&snakeVertices[0],GL_DYNAMIC_DRAW);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glBufferData(GL_ARRAY_BUFFER,1201*3*sizeof(int),nullptr,GL_DYNAMIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1,&EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 1201*3*sizeof(int), nullptr, GL_DYNAMIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER,0);
 	glBindVertexArray(0);
 	glGenVertexArrays(1, &VAO[2]);
 	glGenBuffers(1,&VBO[2]);
 	//bind buffer and vertex array
+
 	glBindVertexArray(VAO[2]);
 	glBindBuffer(GL_ARRAY_BUFFER,VBO[2]);
 	//aquire the vertices data
-	glBufferData(GL_ARRAY_BUFFER,sizeof(fruitVertices),&fruitVertices[0],GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER,3*4*sizeof(GL_FLOAT),nullptr,GL_DYNAMIC_DRAW);
 	//specify the data format
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	//enable the VAO index 0
 	glEnableVertexAttribArray(0);
 
 	//bind to 0 to reset
 	glBindBuffer(GL_ARRAY_BUFFER,0);
 	glBindVertexArray(0);
-
+	
 
 	//set colour to colour buffer
 	glClearColor(0.00f,0.00f,0.00f,1.00f);
@@ -305,8 +315,9 @@ int main()
     que.push_back({19,11});
     que.push_back({19,12});
     que.push_back({19,13});
-
+	
 	std::cout<<"START\n";
+	bool isStarted = false;
 	while(!glfwWindowShouldClose(window))
 	{
 
@@ -321,13 +332,20 @@ int main()
 		//draw the triangles into rectangle
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		//the game logic
-		if(!gameOver) runtime();
-		else 
+		if(!gameOver && isStarted) runtime();
+		else if(gameOver) 
 		{
 			std::cout<<"GAME OVER\n";
 			break;
 		}
+		else
+		{
+			if(GetKeyState('W') < 0 || GetKeyState('S') < 0 || GetKeyState('A') < 0 || GetKeyState('D') < 0) isStarted=true;
+		}
 		snakeVertices.clear();
+		snakeIndices.clear();
+		fruitVertices.clear();
+		float posX,posY;
 		for(int i=29;i>=0;--i) 
             {
                 for(int j=0;j<40;++j) 
@@ -335,42 +353,75 @@ int main()
                     if(tab[j][i]) 
 					{
 						std::cout<<"#"; 			// i=y j=x grid:40x30 GL:1.8x1.35 increment:0.045
+						posX=j*increment;
+                		posY=i*increment;
 						snakeVertices.push_back
 						({
-								-0.9f+(j*increment),		-0.9f+((i+1)*increment),	0.0f,	1.0f,
-								-0.9f+((j+1)*increment),	-0.9f+((i+1)*increment), 	0.0f, 	1.0f,
-								-0.9f+(j*increment),		-0.9f+(i*increment),		0.0f,	1.0f,
-								-0.9f+((j+1)*increment),	-0.9f+(i*increment),		0.0f,	1.0f
+							-0.9f+posX, -0.9f+posY, 0.0f, 
+                        	-0.9f+posX, -0.855f+posY, 0.0f,  
+                        	-0.855f+posX, -0.855f+posY, 0.0f,
+                        	-0.855f+posX, -0.9f+posY, 0.0f
 						});
+						
 						
 					}
 					else if(j==fruitx and i==fruity)		// starting point -0.9 -0.9
                     {										// 
-                        std::cout<<"@";						//
+                        std::cout<<"@";	
+						posX=j*increment;
+                		posY=i*increment;
+						fruitVertices.push_back
+						({
+							-0.9f+posX, -0.9f+posY, 0.0f, 
+                        	-0.9f+posX, -0.855f+posY, 0.0f,  
+                        	-0.855f+posX, -0.855f+posY, 0.0f,
+                        	-0.855f+posX, -0.9f+posY, 0.0f
+						});					//
                     }
                     else std::cout<<".";
                 }
                 std::cout<<"\n";
             }
+
+		for(unsigned int i=0;i<snakeVertices.size()*4;i+=4)
+            {
+                snakeIndices.push_back({
+                
+                    i,i+1,i+3,
+                    i+1,i+3,i+2
+				});
+                
+            }
+		
 		glBindVertexArray(VAO[1]);
 		glUseProgram(shaderProgram2);
 		glBindBuffer(GL_ARRAY_BUFFER,VBO[1]);
-		glBufferData(GL_ARRAY_BUFFER,snakeVertices.size()*4,&snakeVertices[0],GL_DYNAMIC_DRAW);
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, snakeVertices.size()*4);
+		glBufferSubData(GL_ARRAY_BUFFER,0,sizeof(GLfloat)*snakeVertices.size()*12,&snakeVertices[0]);
+		glBindBuffer(GL_ARRAY_BUFFER,EBO);	
+		glBufferSubData(GL_ELEMENT_ARRAY_BUFFER,0,sizeof(unsigned int)*snakeIndices.size()*6,&snakeIndices[0]);
+		glDrawElements(GL_TRIANGLES, snakeIndices.size()*6, GL_UNSIGNED_INT,0);
 		//swap buffers
 
 		glBindVertexArray(VAO[2]);
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindBuffer(GL_ARRAY_BUFFER,VBO[2]);
+		glBufferSubData(GL_ARRAY_BUFFER,0,sizeof(GLfloat)*4*3,&fruitVertices[0]);
+		glDrawArrays(GL_LINE_LOOP, 0, 4);
 
 		glfwSwapBuffers(window);
 		glBindVertexArray(0);
 		//manage glfw events
 		glfwPollEvents();
+		
 	}
 	
 	for(auto j:snakeVertices)
 	{
 		std::cout<<j.x1<<" "<<j.y1<<"\n"<<j.x2<<" "<<j.y2<<"\n"<<j.x3<<" "<<j.y3<<"\n"<<j.x4<<" "<<j.y4<<"\n\n";
+	}	
+
+	for(auto j:snakeIndices)
+	{
+		std::cout<<j.i1<<" "<<j.i2<<" "<<j.i3<<"\n"<<j.i4<<" "<<j.i5<<" "<<j.i6<<"\n\n";
 	}	
 
 	
